3f06d7a4857e32850cfd5873f5366a0e41dff940
cjwinter <cjwinter@CS-CLINTON>
2017-09-08 09:51:21 -0500

    Beginning Commit with some c# classes

---
+++ StockMarket.Math/math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarket
+{
+    public class Math
+    {
+
+        /// <summary>
+        /// Initialize the math class
+        /// </summary>
+        public Math()
+        {
+
+        }
+
+        public class Probabilities
+        {
+            public Probabilities()
+            {
+
+            }
+        }
+
+    }
+}
