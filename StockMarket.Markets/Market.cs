dcca0135fcbd6c5325a3af17471bb6d264ca7c68
Cameron Winter <cjwinter@ksu.edu>
2017-09-08 14:50:04 -0500

    Added Geography and Market

---
+++ StockMarket.Markets/Market.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockMarket;
+
+namespace StockMarket.Markets
+{
+    public class Market
+    {
+        public Market()
+        {
+
+        }
+
+        private double totalAddressableMarket;
+        private double economicInfluence;
+        private StockMarket.Geography.Location primaryLocation;
+
+
+    }
+}
