cbd8c4c6283390d8ec2b3e36b7c42d80310c4625
dhriffel <31215851+dhriffel@users.noreply.github.com>
2017-09-08 15:39:17 -0500

    Company Class Edits. Method for assigning initial probability started.

---
+++ StockMarket.Company/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarket.Company
+{
+    public class Type
+    {
+        private string _name;
+        public Type(string typename)
+        {
+            _name = typename;
+        }
+        public String name
+        {
+            get
+            {
+                return (_name);
+            }
+        }
+        public Double StartProbability()
+        {
+            switch (_name)
+            {
+                case ("Startup"):
+                    
+                    break;
+                case ("SmallBusiness"):
+
+                    break;
+                case ("LargeBusiness"):
+
+                    break;
+                case ("Corporation"):
+
+                    break;
+                case ("Monopoly"):
+
+                    break;
+                default:
+
+
+            }
+        }
+    }
+    
+    /// <summary>
+    /// Class for individual Company data.
+    /// </summary>
+    public class Company
+    {
+        private Double _startupProbability = 0.85;
+
+    }
+    /// <summary>
+    /// Class for holding multiple Company objects.
+    /// </summary>
+    public class Companies
+    {
+
+    }
+}
